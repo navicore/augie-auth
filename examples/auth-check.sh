8fbe4a8d588f66f4209c398fe1514c597b276709
Ed Sweeney <ed@onextent.com>
2019-07-01 12:52:11 -0700

    checkauth

---
+++ examples/auth-check.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+curl -i --request GET \
+  --url http://localhost:3000/api/auth \
+  --cookie auth=ab4DgW48HzwGaR/v7wMWXOd3SqLKwv2yfLyTIbpUnx30z3uNiPwAcpTkV//iJw7UrefFg/ZHBmieIiFjAYdLyhcgZT86xdW/byH8QacYRyo0wbr+O1zZPvBRuZwtYh9pVZkC6/C891ncBuyIC9/X8u+hSBhaeDMsNH4ixZ5oiVZEkAx9FDeAQNLFINCxQx/0qK9ZirGFa9gqDLAp6m9aZhXyw3M3eNmWB1mZ2dw5Pu4kmpHvReqcPNjWhVPQTFA+kXIXhEFw6vB+0+qsV/nYLHuATHoodct1wmx9aR1v0ftS28EoMyZvFg==
---
+++ examples/auth-delete.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+curl -i --request DELETE --url http://localhost:3000/api/auth
